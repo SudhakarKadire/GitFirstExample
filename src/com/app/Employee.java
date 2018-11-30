36fe03323b4f1604ac78816e7700ef9eca1adacb
user <user@192.168.0.10>
2018-11-30 17:36:18 +0530

    sdgfsdfg

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -8,28 +8,5 @@ public class Employee {
 	public Employee() {
 		super();
 	}
-	public int getEmpId() {
-		return empId;
-	}
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
-	public String getEmpName() {
-		return empName;
-	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-	public String getEmpSal() {
-		return empSal;
-	}
-	public void setEmpSal(String empSal) {
-		this.empSal = empSal;
-	}
-	@Override
-	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
-	}
 	
-
 }
